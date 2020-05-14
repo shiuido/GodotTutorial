1c3401dc86697dfa5f8852c54ccf96b30e85ef50
Shiuido <Shiuido@gmail.com>
2020-05-14 19:38:39 +0800

    Changed to touch controls

--- Player.gd
+++ Player.gd
@@ -7,6 +7,9 @@ signal hit
 export var speed = 400
 var screen_size
 
+var velocity = Vector2()
+var target = Vector2()
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	hide()
@@ -15,29 +18,14 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2()
-	
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-		
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-		
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-		
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-		
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
+	if position.distance_to(target) > 10:
+		velocity = (target - position).normalized()
 		$AnimatedSprite.play()
 	else:
+		velocity = Vector2()
 		$AnimatedSprite.stop()
-		
-	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
+	
+	position += velocity * speed * delta
 	
 	if velocity.x != 0:
 		$AnimatedSprite.animation = "right"
@@ -47,6 +35,10 @@ func _process(delta):
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
 
+func _input(event):
+	if event is InputEventScreenTouch and event.pressed:
+		target = event.position
+
 func _on_Player_body_entered(body):
 	$CollisionShape2D.set_deferred("disabled", true)
 	hide()
@@ -54,5 +46,6 @@ func _on_Player_body_entered(body):
 	
 func start(pos):
 	position = pos
+	target = pos
 	show()
 	$CollisionShape2D.disabled = false
